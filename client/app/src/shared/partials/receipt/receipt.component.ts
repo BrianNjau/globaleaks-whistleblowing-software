e8d7610a2169fb675acb628fefce53a729f8ae7d
Brian <brian.wahinya.njau@huawei.com>
2025-07-22 22:57:40 +0300

    Add Channel to report jump

--- client/app/src/shared/partials/receipt/receipt.component.ts
+++ client/app/src/shared/partials/receipt/receipt.component.ts
@@ -4,6 +4,7 @@ import {AppDataService} from "@app/app-data.service";
 import {UtilsService} from "@app/shared/services/utils.service";
 import {FormsModule} from "@angular/forms";
 import {ReceiptValidatorDirective} from "../../directive/receipt-validator.directive";
+import { Router } from "@angular/router";
 
 import {TranslateModule} from "@ngx-translate/core";
 import {TranslatorPipe} from "@app/shared/pipes/translate";
@@ -18,11 +19,13 @@ export class ReceiptComponent{
   protected utilsService = inject(UtilsService);
   protected authenticationService = inject(AuthenticationService);
   protected appDataService = inject(AppDataService);
+  private router = inject(Router);
 
   formattedReceipt = "";
 
   viewReport() {
     this.authenticationService.login(0, 'whistleblower', this.formattedReceipt);
+    this.router.navigate(["/#/"]);
     this.formattedReceipt = ""
   }
 }
